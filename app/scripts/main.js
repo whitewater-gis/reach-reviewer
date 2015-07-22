f680cc306b724e99901a3053af241b620766d3bb
unknown <joel5174@OLY-MCCUNE7L.esri.com>
2015-07-21 17:57:03 -0700

    initial commit...kind of a train wreck right now

---
+++ app/scripts/main.js
@@ -0,0 +1,44 @@
+angular.module('reach-reviewer', ['esri.map'])
+
+  .controller('AppController', function($scope){
+    $scope.invalid_message = 'None yet!';
+  })
+
+  .controller('InvalidController', ['$http', function($http){
+
+    // url
+    var getIdsUrl = 'http://services.arcgis.com/2zRtyrQ6q4mGrLJK/arcgis/rest/services/southeast_review_map/FeatureServer/5/query?' +
+      'where=1%3D1' +
+      '&outFields=name_river, name_section, reach_id, ' +
+      '&orderByFields=name_river' +
+      '&f=json' +
+      '&token=';
+
+    // reference to this to expose inside inner scope
+    var invalid = this;
+
+    // initialize empty array for the reach id's and the actual reach objects
+    invalid.reaches = [];
+
+    // make rest call to get list of invalid reach id's
+    $http.get(getIdsUrl).success(function(data){
+
+      // extract the attributes for each feature and append into list
+      for (var i = 0; i < data.features.length; i++){
+        invalid.reaches.push(data.features[i].attributes);
+      }
+
+    });
+
+
+  }])
+
+  .controller('MapController', function($scope){
+    $scope.map = {
+      center: {
+        lng: -122.676207,
+        lat: 45.523452
+      },
+      zoom: 12
+    };
+  });
